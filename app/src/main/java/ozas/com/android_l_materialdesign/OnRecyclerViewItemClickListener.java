20bd6f84615232cc2dcb02b4442da6c3a96324e4
Ashish Sharma <technologist.ashish@gmail.com>
2014-07-09 21:32:46 +0800

    Updated files for RecyclerViewItemClickListener

---
+++ app/src/main/java/ozas/com/android_l_materialdesign/OnRecyclerViewItemClickListener.java
@@ -0,0 +1,10 @@
+package ozas.com.android_l_materialdesign;
+
+import android.view.View;
+
+/**
+ * Created by ashish.sharma on 9/7/2014.
+ */
+public interface OnRecyclerViewItemClickListener<Employee> {
+    public void onItemClick(View view, Employee employee);
+}
